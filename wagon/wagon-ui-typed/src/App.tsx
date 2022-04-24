f420f7271a2d025d290e271b5f76bdb9da0f4a89
paolomococci <paolomococci@gmail.com>
2022-04-24 20:09:31 +0200

    I added file App.tsx to project wagon-ui-typed

---
+++ wagon/wagon-ui-typed/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+      <main>
+        <section>
+          <h3>todo</h3>
+          <p>...</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
