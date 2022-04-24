8d959d08473b940154c782c73acf057f4d381c59
paolomococci <paolomococci@gmail.com>
2022-04-24 17:12:01 +0200

    added class WagonApplicationTests belonging to project wagon-api

---
+++ wagon/wagon-api/src/test/java/local/example/wagon/WagonApplicationTests.java
@@ -0,0 +1,12 @@
+package local.example.wagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagonApplicationTests {
+
+	@Test
+	void voidTest() {
+	}
+}
