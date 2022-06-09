6ab97b0c247f2b871f2a8106ae1e02355571537b
paolomococci <paolomococci@gmail.com>
2022-06-09 21:38:07 +0200

    I have updated app.module.ts

--- ecart/src/app/app.module.ts
+++ ecart/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { KindComponent } from './components/kind/kind.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    KindComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
