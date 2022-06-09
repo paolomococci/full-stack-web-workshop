0cf4112f93f15d4ea85054581f0f9b1e5a883ba9
paolomococci <paolomococci@gmail.com>
2022-06-09 21:52:03 +0200

    I added file item-alert.component.ts to project ecart

---
+++ ecart/src/app/components/item-alert/item-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-alert',
+  templateUrl: './item-alert.component.html',
+  styleUrls: ['./item-alert.component.sass']
+})
+export class ItemAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
