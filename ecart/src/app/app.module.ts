21481280adcbb785426ab42f6631fec869ca4bb3
paolomococci <paolomococci@gmail.com>
2022-06-09 21:53:08 +0200

    I have updated app.module.ts

--- ecart/src/app/app.module.ts
+++ ecart/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { CartComponent } from './components/cart/cart.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CartComponent } from './components/cart/cart.component'
+import { ToolbarComponent } from './components/toolbar/toolbar.component'
 import { ItemAlertComponent } from './components/item-alert/item-alert.component'
 
 @NgModule({
