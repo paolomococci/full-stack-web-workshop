bce7b2462b7de474657b57085465a10f9f9b4468
paolomococci <paolomococci@gmail.com>
2022-06-09 22:03:40 +0200

    I added file item-shipment.component.ts to project ecart

---
+++ ecart/src/app/components/item-shipment/item-shipment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-shipment',
+  templateUrl: './item-shipment.component.html',
+  styleUrls: ['./item-shipment.component.sass']
+})
+export class ItemShipmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
