9e29f68a615ea28a24d2b6caa60a162dd50dc038
paolomococci <paolomococci@gmail.com>
2022-06-11 21:02:16 +0200

    I have updated product-list.ts

--- ecart/src/persistence/product-list.ts
+++ ecart/src/persistence/product-list.ts
@@ -2,79 +2,79 @@ import { Product } from 'src/app/models/product.model'
 
 export const products: Product[] = [
   {
-    id: 11000001,
+    id: 1,
     name: 'apples',
     price: 2.50,
     description: 'apples from Campania'
   },
   {
-    id: 11000002,
+    id: 2,
     name: 'bananas',
     price: 1.90,
     description: 'bananas from Thailand'
   },
   {
-    id: 11000003,
+    id: 3,
     name: 'pineapple',
     price: 2.20,
     description: 'pineapple from Thailand'
   },
   {
-    id: 11000004,
+    id: 4,
     name: 'jasmine rice',
     price: 2.60,
     description: 'jasmine rice from Thailand'
   },
   {
-    id: 11000005,
+    id: 5,
     name: 'wheat flour',
     price: 1.80,
     description: 'wheat flour from Thailand'
   },
   {
-    id: 11000006,
+    id: 6,
     name: 'coriander',
     price: 0.80,
     description: 'coriander from Thailand'
   },
   {
-    id: 11000007,
+    id: 7,
     name: 'coriander',
     price: 0.50,
     description: 'coriander from Sicily'
   },
   {
-    id: 11000008,
+    id: 8,
     name: 'basil',
     price: 1.70,
     description: 'basil from Liguria'
   },
   {
-    id: 11000009,
+    id: 9,
     name: 'olive oil',
     price: 2.50,
     description: 'olive oil from Umbria'
   },
   {
-    id: 11000010,
+    id: 10,
     name: 'olive oil',
     price: 2.50,
     description: 'olive oil from Liguria'
   },
   {
-    id: 11000011,
+    id: 11,
     name: 'olive oil',
     price: 2.50,
     description: 'olive oil from Apulia'
   },
   {
-    id: 11000012,
+    id: 12,
     name: 'apples',
     price: 2.55,
     description: 'apples from Trentino'
   },
   {
-    id: 11000013,
+    id: 13,
     name: 'bream onions',
     price: 1.88,
     description: 'bream onions from Umbria'
