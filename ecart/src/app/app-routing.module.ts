6a12120c3bba711b96f07e2b0b65de3599e93a8c
paolomococci <paolomococci@gmail.com>
2022-06-11 07:38:43 +0200

    I have updated app-routing.module.ts

--- ecart/src/app/app-routing.module.ts
+++ ecart/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { ItemDetailComponent } from './components/item-detail/item-detail.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ItemListComponent } from './components/item-list/item-list.component'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+
+import { ItemListComponent } from './components/item-list/item-list.component'
+import { ItemDetailComponent } from './components/item-detail/item-detail.component'
+import { CartComponent } from './components/cart/cart.component'
+import { ItemShipmentComponent } from './components/item-shipment/item-shipment.component'
 
 const routes: Routes = [
   {
@@ -21,6 +23,10 @@ const routes: Routes = [
   {
     path: 'bag',
     component: CartComponent
+  },
+  {
+    path: 'shipment',
+    component: ItemShipmentComponent
   },
 ]
 
