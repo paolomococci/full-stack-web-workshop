9ee26c4002ef588fd3fff11c02df4794646cbf2f
paolomococci <paolomococci@gmail.com>
2022-06-11 18:06:20 +0200

    I have updated shipping-list.ts

---
+++ ecart/src/persistence/shipping-list.ts
@@ -0,0 +1,28 @@
+import { Shipment } from 'src/app/models/shipment.model'
+
+export const products: Shipment[] = [
+  {
+    "type": "in the day",
+    "price": 6.00
+  },
+  {
+    "type": "by tomorrow",
+    "price": 3.00
+  },
+  {
+    "type": "in two days",
+    "price": 2.50
+  },
+  {
+    "type": "within three days",
+    "price": 2.00
+  },
+  {
+    "type": "in week",
+    "price": 1.10
+  },
+  {
+    "type": "within the month",
+    "price": 0.50
+  }
+]
