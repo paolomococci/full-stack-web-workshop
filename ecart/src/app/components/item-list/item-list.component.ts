f032839f9530e951bd0f6e0d8ff720f46e9755c1
paolomococci <paolomococci@gmail.com>
2022-06-11 21:01:54 +0200

    I have updated item-list.component.ts

--- ecart/src/app/components/item-list/item-list.component.ts
+++ ecart/src/app/components/item-list/item-list.component.ts
@@ -3,6 +3,8 @@ import {
   OnInit
 } from '@angular/core'
 
+import { products } from 'src/persistence/product-list'
+
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
@@ -10,9 +12,19 @@ import {
 })
 export class ItemListComponent implements OnInit {
 
+  items = products
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onNotify(): void {
+    window.alert('the item is not currently in stock')
+  }
+
+  share(): void {
+    window.alert('the item has been shared')
   }
 
 }
