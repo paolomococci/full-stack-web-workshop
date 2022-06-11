250a036ccd98d410b7068a6b80b8bce0a14e8fec
paolomococci <paolomococci@gmail.com>
2022-06-11 07:58:13 +0200

    I have updated ecart.service.ts

--- ecart/src/app/services/ecart.service.ts
+++ ecart/src/app/services/ecart.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Product } from 'src/app/models/product.model'
 
 @Injectable({
   providedIn: 'root'
