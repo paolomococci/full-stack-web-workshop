be2ca8797697cbb2f4007c81728b9c216e983d84
paolomococci <paolomococci@gmail.com>
2022-07-02 22:57:12 +0200

    I have updated app.module.ts

--- ecart-improved/src/app/app.module.ts
+++ ecart-improved/src/app/app.module.ts
@@ -11,14 +11,15 @@ import { LayoutModule } from '@angular/cdk/layout'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
-import { MatListModule } from '@angular/material/list';
-import { MainComponent } from './components/main/main.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { ItemAlertComponent } from './components/item-alert/item-alert.component';
-import { ItemDetailComponent } from './components/item-detail/item-detail.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
+import { MatListModule } from '@angular/material/list'
+
+import { MainComponent } from './components/main/main.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { CartComponent } from './components/cart/cart.component'
+import { ToolbarComponent } from './components/toolbar/toolbar.component'
+import { ItemAlertComponent } from './components/item-alert/item-alert.component'
+import { ItemDetailComponent } from './components/item-detail/item-detail.component'
+import { ItemListComponent } from './components/item-list/item-list.component'
 import { ItemShipmentComponent } from './components/item-shipment/item-shipment.component'
 
 @NgModule({
