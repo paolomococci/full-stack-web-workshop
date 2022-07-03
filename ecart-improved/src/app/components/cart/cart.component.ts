431cf9a6d93a3a35df6873571969b4a73d8d440e
paolomococci <paolomococci@gmail.com>
2022-07-03 07:45:21 +0200

    I have updated cart.component.ts

--- ecart-improved/src/app/components/cart/cart.component.ts
+++ ecart-improved/src/app/components/cart/cart.component.ts
@@ -2,6 +2,11 @@ import {
   Component,
   OnInit
 } from '@angular/core'
+import { FormBuilder } from '@angular/forms'
+import { Product } from 'src/app/models/product.model'
+
+import { EcartService } from '../../services/ecart.service'
+import { Customer } from 'src/app/models/customer.model'
 
 @Component({
   selector: 'app-cart',
@@ -10,9 +15,27 @@ import {
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ecartService: EcartService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  items: Product[] = this.ecartService.getItems()
+
+  checkoutForm: any = this.formBuilder.group(
+    {
+      name: '',
+      address: ''
+    }
+  )
+
+  onSubmit(): void {
+    this.items = this.ecartService.emptyTheCart()
+    console.log('order sent: ', this.checkoutForm.value)
+    this.checkoutForm.reset()
   }
 
 }
