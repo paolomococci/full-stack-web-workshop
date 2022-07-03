3fd45097789fe198c1e1187b2a6b256486710388
paolomococci <paolomococci@gmail.com>
2022-07-03 07:19:33 +0200

    I have updated item.service.ts

--- ecart-improved/src/app/services/item.service.ts
+++ ecart-improved/src/app/services/item.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Product } from './../models/product.model'
+import {
+  map,
+  Observable
+} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  private path: string = '/assets/items.json'
+
+  products: Product[] = []
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getItems(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.path)
+  }
+
+  getItemArray(): Product[] {
+    this.getItems().subscribe(
+      products => {
+        this.products = products
+      }
+    )
+    return this.products
+  }
 }
