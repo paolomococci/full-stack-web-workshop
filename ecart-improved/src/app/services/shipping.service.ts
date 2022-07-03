10e673fcbe5228c3a0eaac518e7b0c79457f9447
paolomococci <paolomococci@gmail.com>
2022-07-03 07:19:42 +0200

    I have updated shipping.service.ts

--- ecart-improved/src/app/services/shipping.service.ts
+++ ecart-improved/src/app/services/shipping.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Shipment } from '../models/shipment.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShippingService {
 
-  constructor() { }
+  shipments: Shipment[] = []
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Shipment[] {
+    return this.shipments
+  }
+
+  get(index: number): Shipment {
+    return this.shipments[index]
+  }
+
+  push(shipment: Shipment): void {
+    this.shipments.push(shipment)
+  }
+
+  pop(): Shipment | undefined {
+    return this.shipments.pop()
+  }
+
+  length(): number {
+    return this.shipments.length
+  }
 }
