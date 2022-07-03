4ba41d388b4a313b6f2951119e52eac6f6bc0f7e
paolomococci <paolomococci@gmail.com>
2022-07-03 07:19:23 +0200

    I have updated ecart.service.ts

--- ecart-improved/src/app/services/ecart.service.ts
+++ ecart-improved/src/app/services/ecart.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Product } from './../models/product.model'
+import { Shipment } from './../models/shipment.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EcartService {
 
-  constructor() { }
+  private path: string = '/assets/shipment.json'
+
+  items: Product[] = []
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getItems(): Product[] {
+    return this.items
+  }
+
+  getShipmentPrices(): any {
+    return this.http.get<Shipment[]>(this.path)
+  }
+
+  addToCart(product: Product): void {
+    this.items.push(product)
+  }
+
+  emptyTheCart(): Product[] {
+    this.items = []
+    return this.items
+  }
 }
