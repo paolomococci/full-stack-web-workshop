023164d7718bff56dcb42ff01e4c2b70e95c92ec
paolomococci <paolomococci@gmail.com>
2022-07-03 07:44:17 +0200

    I have updated app-routing.module.ts

--- ecart-improved/src/app/app-routing.module.ts
+++ ecart-improved/src/app/app-routing.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import {
+  RouterModule,
+  Routes
+} from '@angular/router'
+
+import { ItemListComponent } from './components/item-list/item-list.component'
+import { ItemDetailComponent } from './components/item-detail/item-detail.component'
+import { CartComponent } from './components/cart/cart.component'
+import { ItemShipmentComponent } from './components/item-shipment/item-shipment.component'
 
 const routes: Routes = [
-  /*
   {
     path: '',
-    redirectTo: '/todo',
+    redirectTo: '/items',
     pathMatch: 'full'
   },
   {
-    path: 'todo',
-    component: TodoComponent
+    path: 'items',
+    component: ItemListComponent
   },
-  */
+  {
+    path: 'items/:itemId',
+    component: ItemDetailComponent
+  },
+  {
+    path: 'bag',
+    component: CartComponent
+  },
+  {
+    path: 'shipment',
+    component: ItemShipmentComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/items',
+    pathMatch: 'full'
+  },
 ]
 
 @NgModule({
