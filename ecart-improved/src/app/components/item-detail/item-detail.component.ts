323b8501149dbeacd11e2764987ae557120cae87
paolomococci <paolomococci@gmail.com>
2022-07-03 07:48:03 +0200

    I have updated item-detail.component.ts

--- ecart-improved/src/app/components/item-detail/item-detail.component.ts
+++ ecart-improved/src/app/components/item-detail/item-detail.component.ts
@@ -2,6 +2,11 @@ import {
   Component,
   OnInit
 } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+
+import { EcartService } from './../../services/ecart.service'
+import { Product } from 'src/app/models/product.model'
+import { ItemService } from './../../services/item.service'
 
 @Component({
   selector: 'app-item-detail',
@@ -10,9 +15,28 @@ import {
 })
 export class ItemDetailComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = this.itemService.getItemArray()
+
+  item: Product | undefined
+
+  constructor(
+    private route: ActivatedRoute,
+    private ecartService: EcartService,
+    private itemService: ItemService
+  ) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap
+    const itemIdFromRoute = Number(routeParams.get('itemId'))
+    console.log(this.products)
+    this.item = this.products.find(
+      item => item.id === itemIdFromRoute
+    )
+  }
+
+  addToCart(item: Product): void {
+    this.ecartService.addToCart(item)
+    window.alert('the item has been placed in your bag')
   }
 
 }
